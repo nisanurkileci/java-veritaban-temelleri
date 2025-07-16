c7b2a775c01efd2a553525d721b8a1a6718b983e
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-07-16 14:48:16 +0300

    [JA-6]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,41 @@ import Config.config;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class Main {
     public static void main(String[] args) {
         Connection conn = config.getConnection();
 
-        String sql = "INSERT INTO users(name, email) VALUES (?, ?)";
+        String sql = "SELECT * FROM users";
 
         try {
+            PreparedStatement pstatment = conn.prepareStatement(sql);
+
+            ResultSet rs = pstatment.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+
+                System.out.println("ID: " + id + ", İsim: " + name + ", Email: " + email);
+            }
+
+            rs.close();
+            pstatment.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println("Hata oluştu!");
+            e.printStackTrace();
+        }
+    }
+}
+
+
+//JA-5
+/* try {
 
             PreparedStatement pstmt = conn.prepareStatement(sql);
 
@@ -31,6 +57,4 @@ public class Main {
             System.out.println("Hata oluştu!");
             e.printStackTrace();
         }
-
-    }
-}
+*/
