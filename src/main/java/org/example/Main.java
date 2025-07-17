82b73346874f868290f7851d63c64dca4c8cf8da
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-07-17 12:30:31 +0300

    [JA-15]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,37 @@
 package org.example;
-
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import user.User;
 import util.HibernateUtil;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
 
 
-            User user = new User();
-
-            user.setName("hakann");
-            user.setEmail("hh@hh.com");
-
-            session.save(user);
-
-            transaction.commit();
-
-            System.out.println("Kullanıcı başarıyla eklendi: " + user);
-
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
+        Long userId = 1L;
+        User user = session.get(User.class, userId);
+        if (user != null) {
+            System.out.println("Tek Kayıt -> ID: " + user.getId() + ", Name: " + user.getName());
+        } else {
+            System.out.println("Kullanıcı bulunamadı.");
         }
+
+        List<User> userList = session.createQuery("from User", User.class).list();
+        System.out.println("\nTüm Kullanıcılar:");
+        for (User u : userList) {
+            System.out.println("ID: " + u.getId() + ", Name: " + u.getName());
+        }
+
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
     }
 
 }
