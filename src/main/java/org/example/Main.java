888ceb5e6d38b097faa65a329781244e1b816f12
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-07-17 09:19:11 +0300

    [JA-10]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,38 @@
 package org.example;
-import config.DataBaseConfig;
 import config.DataBaseConnectorConfig;
 import dao.UserDAOImpl;
-
+import dao.UserDAO;
+import user.User;
 import java.sql.Connection;
+import java.util.List;
+
+import static dao.UserDAOImpl.*;
 
 public class Main {
     public static void main(String[] args) {
 
         DataBaseConnectorConfig.setConnection();
         Connection connection = DataBaseConnectorConfig.getConnection();
-        UserDAOImpl userDAO = new UserDAOImpl(connection);
+
+        UserDAO userDAO = new UserDAOImpl(connection);
+
+        userDAO.createTable();
+
+        User newuser = new User();
+        newuser.setName("Nisa");
+        newuser.setEmail("nn@nn.com");
+
+        userDAO.saveUser(newuser);
 
 
+
+        List<User> users = userDAO.findAll();
+        System.out.println("Tüm Kullanıcılar");
+        for(User u: users){
+            System.out.println(" " + u.getId() + " " + u.getName() + " " + u.getEmail());
+        }
+
+        DataBaseConnectorConfig.closeConnection();
     }
 }
 
