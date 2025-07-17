afbb27442e7898c841a742ec0e142c4f9d0d0323
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-07-17 10:15:53 +0300

    [JA-12]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,14 @@
 package org.example;
-import config.DataBaseConnectorConfig;
-import dao.UserDAOImpl;
-import dao.UserDAO;
-import user.User;
-import java.sql.Connection;
-import java.util.List;
 
-import static dao.UserDAOImpl.*;
+import org.hibernate.Session;
+import util.HibernateUtil;
 
 public class Main {
     public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        System.out.println("Hibernate bağlantısı başarılı!");
+        session.close();
 
-        DataBaseConnectorConfig.setConnection();
-        Connection connection = DataBaseConnectorConfig.getConnection();
-
-        UserDAO userDAO = new UserDAOImpl(connection);
-
-        userDAO.createTable();
-
-        User newuser = new User();
-        newuser.setName("Nisa");
-        newuser.setEmail("nn@nn.com");
-
-        userDAO.saveUser(newuser);
-
-
-
-        List<User> users = userDAO.findAll();
-        System.out.println("Tüm Kullanıcılar");
-        for(User u: users){
-            System.out.println(" " + u.getId() + " " + u.getName() + " " + u.getEmail());
-        }
-
-        DataBaseConnectorConfig.closeConnection();
+        HibernateUtil.shutdown();
     }
 }
-
---
+++ src/main/java/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+package util;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("SessionFactory oluşturulamadı. " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
