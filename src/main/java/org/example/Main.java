8b23c9dc10ba9193a735749dd4a509d75ebfe20d
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-07-17 11:34:51 +0300

    [JA-14]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,34 @@
 package org.example;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import user.User;
 import util.HibernateUtil;
 
 public class Main {
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
-        User user = new User("Nisa", "nisa@orm.com");
-        session.save(user);
 
-        session.getTransaction().commit();
-        session.close();
-        HibernateUtil.shutdown();
+            User user = new User();
 
+            user.setName("hakann");
+            user.setEmail("hh@hh.com");
+
+            session.save(user);
+
+            transaction.commit();
+
+            System.out.println("Kullanıcı başarıyla eklendi: " + user);
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
+
 }
